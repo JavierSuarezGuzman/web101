76739612aa5cbf80cc91d796118740220e2b5cac
JavierSuarezCL <javier.suarez@ciisa.cl>
2021-07-31 14:19:23 -0400

    second commit

---
+++ 2. HTML&JS 20.05.2020/README.markdown
@@ -0,0 +1,5 @@
+# Tarea1-HTML&JS
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/JavierSuarezGuzman/pen/dyYrKLX](https://codepen.io/JavierSuarezGuzman/pen/dyYrKLX).
+
+
---
+++ 2. HTML&JS 20.05.2020/src/script.js
@@ -0,0 +1,6 @@
+function sumar() {
+  var n1 =   document.frmSuma.txtNum1.value;  
+  var n2 =   document.frmSuma.txtNum2.value;
+  var suma = Number(n1) + Number(n2);
+  alert ("La suma es " + suma ) ;
+}
